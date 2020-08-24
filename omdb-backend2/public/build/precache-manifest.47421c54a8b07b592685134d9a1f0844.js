69c66daaf1b7eedc8f007b8db7f2209d39879480
chetana02 <cyelukati@gmail.com>
2020-08-24 14:02:40 +0530

    new public

--- omdb-backend2/public/build/precache-manifest.47421c54a8b07b592685134d9a1f0844.js
+++ omdb-backend2/public/build/precache-manifest.47421c54a8b07b592685134d9a1f0844.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e25cd9ca04cec16ce4e9e77b597eeace",
+    "revision": "56aea2033d1eb1ff787b20dc2d36d7bd",
     "url": "/index.html"
   },
   {
-    "revision": "e6cb642adb5f514510e5",
+    "revision": "2fafc501f82eb861c2b3",
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "7180f87e1e5eee67527c",
+    "revision": "8532c7fa42d9afb50b41",
     "url": "/static/css/main.a5136d75.chunk.css"
   },
   {
-    "revision": "e6cb642adb5f514510e5",
-    "url": "/static/js/2.e57da0af.chunk.js"
+    "revision": "2fafc501f82eb861c2b3",
+    "url": "/static/js/2.b35c3433.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.e57da0af.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b35c3433.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7180f87e1e5eee67527c",
-    "url": "/static/js/main.50c128ec.chunk.js"
+    "revision": "8532c7fa42d9afb50b41",
+    "url": "/static/js/main.b6b57570.chunk.js"
   },
   {
     "revision": "9091f47bb88943cb9e61",
--- omdb-backend2/public/build/service-worker.js
+++ omdb-backend2/public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ad92b235e6778769630aa5b36536683.js"
+  "/precache-manifest.47421c54a8b07b592685134d9a1f0844.js"
 );
 
 self.addEventListener('message', (event) => {
