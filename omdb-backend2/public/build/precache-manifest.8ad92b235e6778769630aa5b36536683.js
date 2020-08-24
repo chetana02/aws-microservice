6aaf9ca5b3d41c2013538d4e3f7402dc181e7639
chetana02 <cyelukati@gmail.com>
2020-08-24 09:33:02 +0530

    first commit

---
+++ omdb-backend2/public/build/precache-manifest.8ad92b235e6778769630aa5b36536683.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25cd9ca04cec16ce4e9e77b597eeace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6cb642adb5f514510e5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7180f87e1e5eee67527c",
+    "url": "/static/css/main.a5136d75.chunk.css"
+  },
+  {
+    "revision": "e6cb642adb5f514510e5",
+    "url": "/static/js/2.e57da0af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e57da0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7180f87e1e5eee67527c",
+    "url": "/static/js/main.50c128ec.chunk.js"
+  },
+  {
+    "revision": "9091f47bb88943cb9e61",
+    "url": "/static/js/runtime-main.89d020cb.js"
+  }
+]);
---
+++ omdb-backend2/routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var axios = require('axios');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    
+    res.send('Express is the best!!!! and so is Chetana');
+    //res.send('Chetana is the best!!!!');
+    
+});
+
+const OMDB_API_KEY = 'e3ec511b';
+const OMDB_URL = 'http://www.omdbapi.com/';
+
+/* IMDB Search */
+router.get('/search', (req, res, next) => {
+    const title = req.query.title
+    const url = `${OMDB_URL}?apikey=${OMDB_API_KEY}&s=${title}`
+    axios.get(url)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(err => {
+            res.send(err)
+        })
+});
+
+/* Single IMDB Movie */
+router.get('/imdb/:imdbId', (req, res, next) => {
+    const imdbId = req.params.imdbId
+    const url = `${OMDB_URL}?apikey=${OMDB_API_KEY}&i=${imdbId}`
+    axios.get(url)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(err => {
+            res.send(err)
+        })
+})
+
+
+module.exports = router;
